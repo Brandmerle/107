6522742b228783f06169a74e095f2ece850f475e
Brandon Merle <brandon.merle@yahoo.com>
2025-01-28 19:59:22 -0500

    Add files via upload

--- 107/test.py
+++ 107/test.py
@@ -57,3 +57,31 @@ me["last_name"] = "Doe" #change a key attribute on the list
 print(me)
 me["color"]= "blue" #add a new key to the list
 print (me)
+
+ages = [32, 74, 20, 69, 52, 26, 31, 77, 43, 73, 51, 57, 19, 79, 40, 34, 27, 23, 21, 44, 53, 55, 24, 36, 41, 47, 78, 46, 68, 75, 49, 83, 61, 60, 29, 56, 67, 17, 70, 81, 87, 38]
+#print the sum of all of the numbers
+def example1():
+    total = 0
+    for age in ages:
+        total=total + age
+    print (total)
+#call the functions
+example1()
+
+#count how many users are equal or older than 21
+def example2():
+    counter = 0
+    for age in ages:
+        if age >= 21:
+            counter +=1
+    print (counter)
+example2()
+
+#how many users are between 30 and 40 yrs old
+def example3():
+    counter = 0
+    for age in ages:
+        if age >=30 and age <=40:
+            counter +=1
+    print ("There are " + str(counter) + " users between 30 and 40")
+example3()
